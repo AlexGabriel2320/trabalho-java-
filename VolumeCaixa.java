8009039d040ed5e9bc102779fa9fbe33f1d145f4
AlexGabriel2320 <alex146g@gmail.com>
2025-05-12 17:26:30 -0300

    Update VolumeCaixa.java

--- VolumeCaixa.java
+++ VolumeCaixa.java
@@ -1,11 +1,11 @@
 package com.mycompany.volumecaixa;
 import java.util.Scanner;
-
+//Faça um programa que leia os valores de COMPRIMENTO, LARGURA e ALTURA e apresente o valor do volume de uma caixa retangular. Utilize para o cálculo a fórmula VOLUME = COMPRIMENTO × LARGURA × ALTURA. em java
 public class VolumeCaixa {
     public static void main(String[] args) {
-        // Leitura dos dados
+        
         try (Scanner scanner = new Scanner(System.in)) {
-            // Leitura dos dados
+            
             System.out.print("Digite o comprimento da caixa: ");
             double comprimento = scanner.nextDouble();
             
@@ -15,10 +15,10 @@ public class VolumeCaixa {
             System.out.print("Digite a altura da caixa: ");
             double altura = scanner.nextDouble();
             
-            // Cálculo do volume
+            
             double volume = comprimento * largura * altura;
             
-            // Exibição do resultado
+           
             System.out.println("O volume da caixa é: " + volume);
         }
     }
